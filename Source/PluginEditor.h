45a948234ffd0f91763d512e155fa35cf0784a57
Zach Howard <obdo32@gmail.com>
2022-02-11 13:59:20 -0800

    refactored peak coefficient generation

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -22,7 +22,9 @@ struct CustomRotarySlider : juce::Slider
 //==============================================================================
 /**
 */
-class BasicEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class BasicEQAudioProcessorEditor  : public juce::AudioProcessorEditor,
+    juce::AudioProcessorParameter::Listener,
+    juce::Timer
 {
 public:
     BasicEQAudioProcessorEditor (BasicEQAudioProcessor&);
@@ -32,10 +34,17 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void parameterValueChanged(int parameterIndex, float newValue) override;
+    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {};
+
+    void timerCallback() override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     BasicEQAudioProcessor& audioProcessor;
+
+    juce::Atomic<bool> parametersChanged {false};
 
     CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
 
